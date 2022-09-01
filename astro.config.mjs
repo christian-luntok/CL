6b18cbf15fa25671049106121f7bc262b2a99e22
Christian <sefluntok@gmail.com>
2022-09-01 23:48:33 +1000

    Update: Reviewed all files to fix the GIT ISSUE that occurred

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
-<<<<<<< HEAD
-import tailwind from "@astrojs/tailwind";
-
-=======
->>>>>>> 63836dd (Update: Updated tailwind config. Implemented Dark and Light Mode)
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-<<<<<<< HEAD
-<<<<<<< HEAD
     tailwind({
       config: {},
     }),
@@ -20,25 +13,4 @@ export default defineConfig({
       external: ["svgo"],
     },
   },
-=======
-    tailwind({}),
-=======
-    tailwind({
-      config: {},
-    }),
-<<<<<<< HEAD
->>>>>>> cade9f7 (Update: Created Title Component & Introduction List partial.)
-  ]
->>>>>>> 3fbea8f (Update: Initial playaround with astro.)
-=======
-  ],
-<<<<<<< HEAD
->>>>>>> 63836dd (Update: Updated tailwind config. Implemented Dark and Light Mode)
-=======
-  vite: {
-    ssr: {
-      external: ["svgo"],
-    },
-  },
->>>>>>> 64311ed (Addition: Finalised Post Layout for posts layout and tried Markdown using the PostLayout)
 });
